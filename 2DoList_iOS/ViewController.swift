748cfbf6ec9c94d6914b5c7a5befeb1e9ae14787
jay1261 <dongjae73@naver.com>
2022-12-29 21:01:43 +0900

    First commit

---
+++ 2DoList_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2DoList_iOS
+//
+//  Created by DongJae Lee on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2DoList_iOSUITests/_DoList_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _DoList_iOSUITestsLaunchTests.swift
+//  2DoList_iOSUITests
+//
+//  Created by DongJae Lee on 2022/12/29.
+//
+
+import XCTest
+
+final class _DoList_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
